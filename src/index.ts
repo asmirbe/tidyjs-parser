c2b82d928e4f4487f9ce463842261338715e559b
asmirbe <asmir.belkic@gmail.com>
2025-03-23 12:36:47 +0100

    refactor(types): réorganiser l'exportation des types dans index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { validateAndFixImportWithBabel } from "./fixer";
 import { ImportParser } from "./parser";
 import { ImportParserError } from "./errors";
 import { DEFAULT_CONFIG } from "./types";
+
 import type { ParserConfig, ParserResult } from "./types";
+
 function parseImports(sourceCode: string, config: ParserConfig): ParserResult {
   const parser = new ImportParser(config);
   const { groups, originalImports, invalidImports } = parser.parse(sourceCode);
@@ -11,6 +13,12 @@ function parseImports(sourceCode: string, config: ParserConfig): ParserResult {
   return { groups, originalImports, appSubfolders, invalidImports };
 }
 
-export { ImportParser, ImportParserError, parseImports, validateAndFixImportWithBabel, DEFAULT_CONFIG };
+export {
+  ImportParser,
+  ImportParserError,
+  parseImports,
+  validateAndFixImportWithBabel,
+  DEFAULT_CONFIG
+};
 
 export * from "./types";
